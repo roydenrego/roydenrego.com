7ea3ed6e9eded25332db255a57cfdeafd744a423
Royden Rego <roydenrego@softrixz.com>
2018-10-31 19:27:31 +0000

    CSS and JS Code Formatting

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -5,7 +5,7 @@
   PRE LOADER
 -------------------------------------------------------------------------------*/
 
-var site_url = "https://roydenrego.com"; 
+var site_url = "https://roydenrego.com";
 
 $(window).load(function() {
   $('.preloader').fadeOut(1000); // set duration in brackets    
@@ -86,14 +86,14 @@ $(document).ready(function() {
 
   $("#contactForm").on('submit', function(e) {
     e.preventDefault();
-    
+
     //let submit_url = site_url + "/submit"
     $.ajax({
       url: '/submit',
       type: "POST",
       cache: false,
       data: $("#contactForm").serialize(),
-      success: function(data){
+      success: function(data) {
         $("#contactForm").trigger('reset')
 
         swal(
@@ -104,31 +104,31 @@ $(document).ready(function() {
       }
     });
   });
-  
+
   //Content Filter
-  
+
   $('input:radio[name="project_type"]').change(
-    function(){
-        if (this.checked) {
-            var type = $(this).attr('id');
-            
-            
-            $(".card").removeClass('card-disappear');
-            
-            if(type == "mobile") {
-              $(".web-design").addClass('card-disappear');
-              $(".workshop").addClass('card-disappear');
-            }
-            else if(type == "web") {
-              $(".mobile-app").addClass('card-disappear');
-              $(".workshop").addClass('card-disappear');
-            }
-            else if(type == "workshop") {
-              $(".mobile-app").addClass('card-disappear');
-              $(".web-design").addClass('card-disappear');
-            }
+    function() {
+      if (this.checked) {
+        var type = $(this).attr('id');
+
+
+        $(".card").removeClass('card-disappear');
+
+        if (type == "mobile") {
+          $(".web-design").addClass('card-disappear');
+          $(".workshop").addClass('card-disappear');
         }
+        else if (type == "web") {
+          $(".mobile-app").addClass('card-disappear');
+          $(".workshop").addClass('card-disappear');
+        }
+        else if (type == "workshop") {
+          $(".mobile-app").addClass('card-disappear');
+          $(".web-design").addClass('card-disappear');
+        }
+      }
     });
-  
-  
+
+
 });
