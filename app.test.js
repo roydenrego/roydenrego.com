b7d2d36c4686d732e1bef6c933c44ac54df0ce9e
Royden Rego <roydenrego@softrixz.com>
2018-11-28 02:42:48 +0000

    Fixed tests not exiting and added deployment to travis

--- app.test.js
+++ app.test.js
@@ -1,7 +1,11 @@
+/* global afterAll */
+
 const request = require('supertest');
 const app = require('./app')
 describe('Test the root path', () => {
     test('It should response the GET method', () => {
         return request(app).get('/').expect(200);
     });
+    
+    afterAll(() => app.mongoose.disconnect());
 })
