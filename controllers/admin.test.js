dfb176bf399fb0270e9b63f1f37939ade6977cd3
Royden Rego <roydenrego@softrixz.com>
2018-12-01 06:11:17 +0000

    Removed dummy commit

--- controllers/admin.test.js
+++ controllers/admin.test.js
@@ -28,7 +28,6 @@ describe(`Test the Admin API's`, () => {
         });
         
     
-    //added dummy comment
     // it('should allow access to admin when logged in', function(done) {
     //     console.log(cookie.toString());
     //     var req = agent
