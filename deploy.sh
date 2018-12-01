a77b72ce739445a2f83e956db2f497ba89bbc4e5
Royden Rego <roydenrego@softrixz.com>
2018-12-01 09:07:05 +0000

    Another possible fix for the build script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 eval "$(ssh-agent -s)"
 pwd
+openssl aes-256-cbc -K $encrypted_26b4962af0e7_key -iv $encrypted_26b4962af0e7_iv -in rr-lsdf-ap-sea.pem.enc -out rr-lsdf-ap-sea.pem -d
 ls
 chmod 600 rr-lsdf-ap-sea.pem
 ssh-add rr-lsdf-ap-sea.pem
